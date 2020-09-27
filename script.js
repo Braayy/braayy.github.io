5bc22a9f5b9b584a5d8229bfbfe4524f65f3cf12
Braayy <brayangomesruel@gmail.com>
2020-09-27 10:53:46 -0300

    using github raw images

--- script.js
+++ script.js
@@ -21,31 +21,31 @@ const vue = new Vue({
             {
                 name: 'bBans',
                 description: 'A simple ban/mute plugin',
-                imageUrl: 'file:///home/braayy/workspace/bBans/gifs/ban.gif',
+                imageUrl: 'https://raw.githubusercontent.com/Braayy/bbans/master/gifs/ban.gif',
                 url: 'https://github.com/braayy/bBans'
             },
             {
                 name: 'bPets',
                 description: 'A pets plugin for Paper 1.12',
-                imageUrl: 'file:///home/braayy/workspace/bPets/gifs/pets.gif',
+                imageUrl: 'https://raw.githubusercontent.com/Braayy/bpets/master/gifs/pets.gif',
                 url: 'https://github.com/braayy/bPets'
             },
             {
                 name: 'bLobby',
                 description: 'A lobby plugin for Paper 1.12',
-                imageUrl: 'file:///home/braayy/workspace/bLobby/gifs/lobby.gif',
+                imageUrl: 'https://raw.githubusercontent.com/Braayy/blobby/master/gifs/lobby.gif',
                 url: 'https://github.com/braayy/bLobby'
             },
             {
                 name: 'bChat',
                 description: 'A chat management plugin for Paper 1.12',
-                imageUrl: 'file:///home/braayy/workspace/bChat/gifs/chat_regional.gif',
+                imageUrl: 'https://raw.githubusercontent.com/Braayy/bchat/master/gifs/chat_regional.gif',
                 url: 'https://github.com/braayy/bChat'
             },
             {
                 name: 'bSpawner',
                 description: 'A simple spawner command plugin',
-                imageUrl: 'file:///home/braayy/workspace/bSpawner/gifs/spawner.gif',
+                imageUrl: 'https://raw.githubusercontent.com/Braayy/bspawner/master/gifs/spawner.gif',
                 url: 'https://github.com/braayy/bSpawner'
             }
         ]
